e6608e930c59857274934d252a0af2a50808c879
xuyuanfan <xuyuanfan77@outlook.com>
2016-07-07 18:32:54 +0800

    增加了运营后台设计（未完成）

--- Application/Runtime/Cache/Home/d421352dfdc946c93d83cd205baf0330.php
+++ Application/Runtime/Cache/Home/d421352dfdc946c93d83cd205baf0330.php
@@ -8,7 +8,7 @@
 		<a>专辑精选</a>
 	</div>
 	<ul>
-		<?php $__FOR_START_13827__=0;$__FOR_END_13827__=3;for($specialIndex=$__FOR_START_13827__;$specialIndex < $__FOR_END_13827__;$specialIndex+=1){ if($specialImage[$specialIndex] != NULL): ?><li>
+		<?php $__FOR_START_9191__=0;$__FOR_END_9191__=3;for($specialIndex=$__FOR_START_9191__;$specialIndex < $__FOR_END_9191__;$specialIndex+=1){ if($specialImage[$specialIndex] != NULL): ?><li>
 					<img class="wSpecial-coverimage" src="<?php echo ($specialImage[$specialIndex]); ?>"/>
 					<a class="wSpecial-maintitle"><?php echo ($specialMaintitle[$specialIndex]); ?></a>
 					<a class="wSpecial-subhead"><?php echo ($specialSubhead[$specialIndex]); ?></a>
