74f428572c4fcb2a58ec1fb035d4c27048f22501
xuyuanfan <xuyuanfan77@outlook.com>
2016-07-15 17:29:54 +0800

    增加两篇文章

--- DataBase/luoji.sql
+++ DataBase/luoji.sql
@@ -10,13 +10,13 @@ Target Server Type    : MYSQL
 Target Server Version : 50617
 File Encoding         : 65001
 
-Date: 2016-07-14 22:11:21
+Date: 2016-07-15 17:24:56
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `article`
+-- Table structure for article
 -- ----------------------------
 DROP TABLE IF EXISTS `article`;
 CREATE TABLE `article` (
@@ -34,15 +34,17 @@ CREATE TABLE `article` (
   `readnum` mediumint(9) unsigned NOT NULL COMMENT '阅读量',
   `createtime` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COMMENT='文章表';
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COMMENT='文章表';
 
 -- ----------------------------
 -- Records of article
 -- ----------------------------
 INSERT INTO `article` VALUES ('23', '23.jpg', '23.jpg', '数据表之间的关系', '一对一关系、一对多关系、多对多关系', '</p></blockquote><p><ul><li><span style=\"line-height: 1.8;\">一对多关系：这是最普通的一种关系。在这种关系中，A表中的一行可以匹配B表中的多行，但是B表中的一行只能匹配A表中的一行。只有当一个相关列是一个主键或具有唯一约束时，才能创建一对多关系。</span></li><li><span style=\"line-height: 1.8;\">多对多关系：A表中的一行可以匹配B表中的多行，反之亦然。要创建这种关系，需要定义第三个表，称为结合表，它的主键由A表和B表的外部键组成。</span></li><li><span style=\"line-height: 1.8;\">一对一关系：A表中的一行最多只能匹配于B表中的一行，反之亦然。如果相关列都是主键或都具有唯一约束，则可以创建一对一关系。这种关系并不常见，因为一般来说，按照这种方式相关的信息可以都在一个表中。</span></li></ul></p>', '12', '1', '0', '0', '0', '0', '2016-07-14 19:52:57');
+INSERT INTO `article` VALUES ('24', '24.png', '24.jpg', '深入理解sql的五种连接', '内连接、左外连接、右外连接、全连接、交叉连接', '<ol><li>内连接：只有两个表相匹配的行才能在结果集中出现</li><li>外连接：包括&nbsp;（1）左连接(左边的表不加限制)&nbsp;（2）右连接(右边的表不加限制)&nbsp;（3）全连接(左右两表都不加限制)</li><li>交叉连接：也叫做笛卡尔积</li></ol>', '12', '1', '0', '0', '0', '0', '2016-07-15 16:47:45');
+INSERT INTO `article` VALUES ('25', '25.png', '25.jpg', 'Mysql常用数据类型', '整数型、小数型、字符串型、时间日期型', '<p>SQL中将数据类型分了四大类：整数型、小数型、字符串型和时间日期型。</p><p><br></p>', '12', '1', '0', '0', '0', '0', '2016-07-15 16:49:42');
 
 -- ----------------------------
--- Table structure for `collect`
+-- Table structure for collect
 -- ----------------------------
 DROP TABLE IF EXISTS `collect`;
 CREATE TABLE `collect` (
@@ -59,7 +61,7 @@ CREATE TABLE `collect` (
 INSERT INTO `collect` VALUES ('12', '23', '2016-07-14 20:08:51');
 
 -- ----------------------------
--- Table structure for `include`
+-- Table structure for include
 -- ----------------------------
 DROP TABLE IF EXISTS `include`;
 CREATE TABLE `include` (
@@ -73,9 +75,12 @@ CREATE TABLE `include` (
 -- ----------------------------
 -- Records of include
 -- ----------------------------
+INSERT INTO `include` VALUES ('1', '23', '2016-07-15 17:07:31');
+INSERT INTO `include` VALUES ('1', '24', '2016-07-15 17:07:38');
+INSERT INTO `include` VALUES ('1', '25', '2016-07-15 17:07:45');
 
 -- ----------------------------
--- Table structure for `manuscript`
+-- Table structure for manuscript
 -- ----------------------------
 DROP TABLE IF EXISTS `manuscript`;
 CREATE TABLE `manuscript` (
@@ -91,15 +96,17 @@ CREATE TABLE `manuscript` (
   `status` tinyint(4) NOT NULL COMMENT '审核状态',
   `createtime` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COMMENT='稿件表';
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8 COMMENT='稿件表';
 
 -- ----------------------------
 -- Records of manuscript
 -- ----------------------------
 INSERT INTO `manuscript` VALUES ('20', '20.jpg', '数据表之间的关系', '一对一关系、一对多关系、多对多关系', '<blockquote><p>数据表之间有三种关系：1、一对一关系；2、一对多关系；3、多对多关系；\r\n</p></blockquote><p><ul><li><span style=\"line-height: 1.8;\">一对多关系：这是最普通的一种关系。在这种关系中，A表中的一行可以匹配B表中的多行，但是B表中的一行只能匹配A表中的一行。只有当一个相关列是一个主键或具有唯一约束时，才能创建一对多关系。</span></li><li><span style=\"line-height: 1.8;\">多对多关系：A表中的一行可以匹配B表中的多行，反之亦然。要创建这种关系，需要定义第三个表，称为结合表，它的主键由A表和B表的外部键组成。</span></li><li><span style=\"line-height: 1.8;\">一对一关系：A表中的一行最多只能匹配于B表中的一行，反之亦然。如果相关列都是主键或都具有唯一约束，则可以创建一对一关系。这种关系并不常见，因为一般来说，按照这种方式相关的信息可以都在一个表中。</span></li></ul></p>', '12', '1', '0', '0', '1', '2016-07-14 19:40:45');
+INSERT INTO `manuscript` VALUES ('21', '21.png', '深入理解sql的五种连接', '内连接、左外连接、右外连接、全连接、交叉连接', '<ol><li>内连接：只有两个表相匹配的行才能在结果集中出现</li><li>外连接：包括&nbsp;（1）左连接(左边的表不加限制)&nbsp;（2）右连接(右边的表不加限制)&nbsp;（3）全连接(左右两表都不加限制)</li><li>交叉连接：也叫做笛卡尔积</li></ol>', '12', '1', '0', '0', '0', '2016-07-15 11:50:23');
+INSERT INTO `manuscript` VALUES ('22', '22.png', 'Mysql常用数据类型', '整数型、小数型、字符串型、时间日期型', '<p>SQL中将数据类型分了四大类：整数型、小数型、字符串型和时间日期型。</p><p><br></p>', '12', '1', '0', '0', '0', '2016-07-15 15:58:43');
 
 -- ----------------------------
--- Table structure for `special`
+-- Table structure for special
 -- ----------------------------
 DROP TABLE IF EXISTS `special`;
 CREATE TABLE `special` (
@@ -120,9 +127,10 @@ CREATE TABLE `special` (
 -- ----------------------------
 -- Records of special
 -- ----------------------------
+INSERT INTO `special` VALUES ('1', '数据的世界', '数据库基础知识', '', '1.jpg', '0', '1', '0', '0', '2016-07-15 16:59:02');
 
 -- ----------------------------
--- Table structure for `user`
+-- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
@@ -139,9 +147,10 @@ CREATE TABLE `user` (
   `createtime` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`),
   KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8 COMMENT='用户表';
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
 INSERT INTO `user` VALUES ('12', 'xuyuanfan1', '4e829d20119f31638dd50f6db5bf514b', '许远帆', 'default.jpg', 'xuyuanfan77@outlook.com', '0', '产品助理', '广东医群科技有限公司', '随心，只为梦想，无所畏惧……', '2016-07-14 16:07:20');
+INSERT INTO `user` VALUES ('13', 'xuyuanfan2', '4e829d20119f31638dd50f6db5bf514b', '远航之帆', '13.jpg', '770534229@qq.com', '0', '产品助理', '广东医群科技有限公司', '随心，只为梦想，无所畏惧……', '2016-07-15 17:13:01');
